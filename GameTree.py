28a38896eab1dc224b5bedbe236f633164a5a6a2
olivierhsta <olivier@hassaoui.com>
2017-03-15 19:45:58 -0400

    Revert "Revert "Arbre de profondeur infinie""
    This reverts commit c6a8ca625816ac1151617d4b88b30d40a9ba9331.

--- GameTree.py
+++ GameTree.py
@@ -1,24 +1,9 @@
 from Node import Node
 from MetaGame import MetaGame
+from ArrayQueue import ArrayQueue
+
 
 class GameTree:
-    """Abstract base representing a tree structure."""
-
-# ------------------------------- nested Position class -------------------------------
-    class Position:
-        """An abstraction representing the location of a single element."""
-
-        def element(self):
-            """Return the element stored at this Position."""
-            raise NotImplementedError('must be implemented by subclass')
-        def eq(self, other):
-            """Return True if other Position represents the same location."""
-            raise NotImplementedError('must be implemented by subclass')
-
-        def ne(self, other):
-            """Return True if other does not represent the same location."""
-            return not (self == other)  # opposite of     eq
-
     def __init__(self, root, depth):
         last_move = root.get_last_move()
         self._root = Node(root)
@@ -26,26 +11,36 @@ class GameTree:
         self._create_children(last_move, self._root, self._depth)
 
     def _create_children(self, last_move, current, n):
-        current_meta_bin = current.get_meta().get_meta_bin()
-        for i in range(0, 80, 9):
-            if i <= last_move < i+9:
-                last_case = self._root.get_meta().get_game(i//9).get_case(last_move % 9)
-        next_game = self._root.get_meta().get_game(last_move % 9)
-        for case in next_game.empty_cases():
-            if last_case.is_x():
-                new_meta_bin = case.get_bin_position() + \
-                               current_meta_bin[7:7+(2 * case.get_position())] + \
-                               '10' + \
-                               current_meta_bin[9+(2 * case.get_position()):170]
-            else:
-                new_meta_bin = case.get_bin_position() + \
-                               current_meta_bin[7:7 + (2 * case.get_position())] + \
-                               '01' + \
-                               current_meta_bin[9 + (2 * case.get_position()):170]
-            current.add_child(Node(MetaGame(str(int(new_meta_bin, 2))), current))
+        if n == 1:
+            return
+        else:
+            current_meta_bin = current.get_meta().get_meta_bin()
+            for i in range(0, 80, 9):
+                if i <= last_move < i + 9:
+                    last_case = current.get_meta().get_game(i // 9).get_case(last_move % 9)
+            next_game = current.get_meta().get_game(last_move % 9)
+            for case in next_game.empty_cases():
+                if last_case.is_x():
+                    new_meta_bin = case.get_bin_position() + \
+                                   current_meta_bin[7:7 + (2 * case.get_position())] + \
+                                   '10' + \
+                                   current_meta_bin[9 + (2 * case.get_position()):170]
+                else:
+                    new_meta_bin = case.get_bin_position() + \
+                                   current_meta_bin[7:7 + (2 * case.get_position())] + \
+                                   '01' + \
+                                   current_meta_bin[9 + (2 * case.get_position()):170]
+                new_child = Node(MetaGame(str(int(new_meta_bin, 2))), current)
+                current.add_child(new_child)
+                self._create_children(case.get_position(), new_child, n - 1)
 
     def __str__(self):
-        str_return = self._root.toString() + '\n'
-        for child in self._root.get_children():
-            str_return += ' ' + child.toString()
+        str_return = ''
+        q_uttt = ArrayQueue()
+        q_uttt.enqueue(self._root)
+        while not q_uttt.is_empty():
+            child = q_uttt.dequeue()
+            str_return += child.get_meta().get_meta_int() + '\n'
+            for grand_child in child.get_children():
+                q_uttt.enqueue(grand_child)
         return str_return
--- Node.py
+++ Node.py
@@ -1,15 +1,18 @@
 class Node:
 
-    def __init__(self, game, parent=None, children=[]):
-        self._meta = game
+    def __init__(self, meta_game, parent=None):
+        self._meta = meta_game
         self._parent = parent
-        self._children = children
+        self._children = []
 
     def add_child(self, child):
         self._children.append(child)
 
     def get_children(self):
         return self._children
+
+    def nb_children(self):
+        return int(len(self._children))
 
     def get_meta(self):
         return self._meta
